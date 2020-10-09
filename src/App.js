474e7a2e6aa1772f3f8bcb88363aac0d839c7fc9
devferx <ferkki@hotmail.com>
2020-10-09 11:25:40 -0400

    Header listo para vista mobile

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div>
-      <header>Header</header>
+      <Header
+        title="IP Address Tracker"
+        ipAddres="192.212.174.101"
+        location="Brooklyn, NY 101001"
+        timezone="-05:00"
+        isp="SpaceX Starlink"
+      />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,42 @@
+import React from "react";
+import arrowIcon from "../assets/images/icon-arrow.svg";
+import "../assets/styles/components/Header.css";
+
+function Header({ title, ipAddres, location, timezone, isp }) {
+  return (
+    <div className="header">
+      <h2 className="header--title">{title}</h2>
+      <form className="header__form">
+        <input
+          className="header__form--input"
+          type="text"
+          placeholder="Search for any IP address or domain"
+        />
+        <button className="header__form--btn">
+          <img src={arrowIcon} alt="arrow right" />
+        </button>
+      </form>
+
+      <div className="header__panel">
+        <div className="header__panel__item">
+          <p className="header__panel__item--title">IP Address</p>
+          <p className="header__panel__item--info">{ipAddres}</p>
+        </div>
+        <div className="header__panel__item">
+          <p className="header__panel__item--title">Location</p>
+          <p className="header__panel__item--info">{location}</p>
+        </div>
+        <div className="header__panel__item">
+          <p className="header__panel__item--title">Timezone</p>
+          <p className="header__panel__item--info">UTC {timezone}</p>
+        </div>
+        <div className="header__panel__item">
+          <p className="header__panel__item--title">ISP</p>
+          <p className="header__panel__item--info">{isp}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
