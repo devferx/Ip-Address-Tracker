afa1cc08c2a473b9a46d66adb0f7172e043847f4
devferx <ferkki@hotmail.com>
2020-10-17 20:05:42 -0400

    Updates

---
+++ precache-manifest.a756f362541b37e5648dc17c82b377d7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57df8a1336fa23be13de2f8a57d679ac",
+    "url": "/Ip-Address-Tracker/index.html"
+  },
+  {
+    "revision": "6d569a86952e529ae388",
+    "url": "/Ip-Address-Tracker/static/css/2.b4a16772.chunk.css"
+  },
+  {
+    "revision": "00efc79e222bb84465a3",
+    "url": "/Ip-Address-Tracker/static/css/main.801b09c3.chunk.css"
+  },
+  {
+    "revision": "6d569a86952e529ae388",
+    "url": "/Ip-Address-Tracker/static/js/2.acebcd7d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Ip-Address-Tracker/static/js/2.acebcd7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00efc79e222bb84465a3",
+    "url": "/Ip-Address-Tracker/static/js/main.e591136d.chunk.js"
+  },
+  {
+    "revision": "0b18aeadb06d77ac495c",
+    "url": "/Ip-Address-Tracker/static/js/runtime-main.4d5ab649.js"
+  },
+  {
+    "revision": "53ef8ed8ef4b92721f00cb8bfdc5c9b9",
+    "url": "/Ip-Address-Tracker/static/media/icon-arrow.53ef8ed8.svg"
+  },
+  {
+    "revision": "ff17146919959b508ee5dd9af2f9f9e5",
+    "url": "/Ip-Address-Tracker/static/media/icon-location.ff171469.svg"
+  },
+  {
+    "revision": "8d3f4ad64bf3a9320e172edf01e0c016",
+    "url": "/Ip-Address-Tracker/static/media/pattern-bg.8d3f4ad6.png"
+  }
+]);
